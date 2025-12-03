3bdb475a32f02a5543444705f3771b7d069a2ad9
Austin Klein <austin@kleindesigns.tech>
2025-12-02 17:08:05 -0900

    Working state for part b
    Answer is output in the format (min_score_found, [csv of separated jumps to finish])

--- jumpit_helpers.py
+++ jumpit_helpers.py
@@ -9,8 +9,10 @@ from enum import Enum
 """ ######################## Game Class ######################### """
 class Game:
     def __init__(self):
-        self.size = 5  # init_game_size_from_user()
+        self.size = init_game_size_from_user()
         self.board = create_board(self.size)
+        self.costs = self.board[0]
+        self.jumps = self.board[1]
 
 class Sample_Game(Game):
     def __init__(self):
@@ -18,6 +20,9 @@ class Sample_Game(Game):
         self._sample = int(init_sample_choice_from_user())
         self.board = get_sample_board(choice=self._sample)
         self.size = get_size_from_board(self.board)
+        self.costs = self.board[0]
+        self.jumps = self.board[1]
+
 
 #####################################################################
 """ $$$$$$$$$$$$$$$$$$$$$$$$ Functions $$$$$$$$$$$$$$$$$$$$$$$$$$ """
@@ -29,7 +34,7 @@ def init_game_size_from_user():
     return int(input("Please enter an integer for the size of the JumpIt gameboard: "))
 
 def init_sample_choice_from_user():
-    return int(input("Which sample board would you like to use? (hint: choices are 1-5): "))
+    return int(input("Which sample board would you like to use? (hint: choices are 1-3): "))
 
 # Create the game board
 def create_board(n: int):
@@ -75,18 +80,35 @@ def get_sample_board(choice: int) -> list:
 def get_size_from_board(board: list):
     return len(board[0])
 
+def get_cost(column: int, g: Game) -> int:
+    return g.board[0][column - 1]  # Index vs Col
+
 """ ######################## ALGORITHMS ######################### """
 # Recursive (Naive) Solution - Dynamic Programming Problem
-def min_cost_jumpit(instance: Game):
-    cost = instance.board[0][0]
-    jump = instance.board[0][1]
+def min_cost_jumpit(col: int, instance: Game):
+    if col == 1:
+        return get_cost(col, instance), [1]
+
+    min_cost = float("inf")
+    best_path = []
+
+    for given_col in range(max(1, col - 3), col):  # Last three since max jump is three
+        if given_col + instance.jumps[given_col - 1] >= col:
+            cost_from_prev, path_from_prev = min_cost_jumpit(given_col, instance)
+            cost_to_here = cost_from_prev + instance.costs[col - 1]
+
+            if cost_to_here < min_cost:
+                min_cost = cost_to_here
+                best_path = path_from_prev + [col]
+
+    return min_cost, best_path
 
 
 """ ########################### ENUMS ############################ """
 class Sample(Enum):
     ONE = [
-        [ 1,  5,  5, 50, 20 ],
-        [ 1,  2,  2,  2,  3 ],
+        [ 10,  3, 87, 58, 57, 10 ],
+        [  1,  2,  3,  1,  1,  1 ],
     ]
     TWO = [
         [ 1, 75, 54, 10, 54, 10, 27, 10, 20 ],
--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import jumpit_helpers as j
 from jumpit_helpers import Game, Sample_Game
 
 # Game Instance
-#Jump_It = Game()
-Jump_It = Sample_Game()
+if int(input("Random (1) or Sample (2) game?\n> ")) == 1:
+    Jump_It = Game()
+else:
+    Jump_It = Sample_Game()
 
+# User Feedback
 j.print_board(Jump_It)
 
-j.min_cost_jumpit(Jump_It)
+# Run function - Size must be passed explicitly for recursion
+cost = j.min_cost_jumpit(Jump_It.size, Jump_It)
+
+print(cost)
